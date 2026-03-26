0651733a21737394ed6d10ee06c143c77384c28a
Pierre Parent <pierre.parent@pparent.fr>
2026-03-26 14:01:56 +0100

    Improve ContentHub Handling

--- utils/filedialog-deamon.sh
+++ utils/filedialog-deamon.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+echo "df84ff50557373cd882941cafb7ad344  /lib/aarch64-linux-gnu/liblomiri-private.so"| md5sum -c -
+#If we are running the latest version of lomiri we'll use Xcb to display ContentHub Windows
+if [ "$?" -eq "0" ]; then
+    export QT_QPA_PLATFORM=xcb
+fi
+
 PID=$1
 needtoexport=0;
 echo "" > /home/phablet/.cache/whatslectron.pparent/exportlock
 allreadynavigated=0
+allreadyseenwindow=0
+firstimport=1;
 
 xev -root  | while read -r _; do
 
@@ -15,6 +23,19 @@ xev -root  | while read -r _; do
     for window in $windows ; do
         count=$(( count + 1 ))
     done
+    
+    if [ "$count" -eq "0" ]; then
+        windows=$(xdotool search --all --pid $PID)
+        for window in $windows ; do
+            count=$(( count + 1 ))
+        done
+        if [ "$count" -eq "0" ]&&  [ "$allreadyseenwindow" -eq "1" ] ; then
+            echo "No window left exiting...";
+            exit 0;
+        fi
+    else
+        allreadyseenwindow=1;
+    fi
     
     if [ "$count" -gt "1" ]; then
         echo "more than one window"
@@ -37,23 +58,37 @@ xev -root  | while read -r _; do
                     needtoexport=$window
                 else
                     echo "Import File"
-                    qmlscene utils/upload-helper/qml/ImportPage.qml -I  utils/upload-helper/;
-                    while read -t 0.01 -r _; do :; done
+                    qmlscene utils/upload-helper/qml/ImportPage.qml -I  utils/upload-helper/ &
+                    pid=$!
+                    while kill -0 "$pid" 2>/dev/null; do
+                        while read -t 0.01 -r _; do :; done
+                    done
                     xdotool windowfocus $window
-                    xdotool sleep 0.1
-                    xdotool key  --window $window Alt+d
-                    xdotool sleep 0.4
-                    xdotool key  --window $window F6
-                    xdotool sleep 0.1
-                    xdotool key  --window $window F6
-                    xdotool sleep 0.1                    
-                    xdotool key  --window $window KP_Enter      
-                    xdotool sleep 0.4
-                    xdotool key  --window $window KP_Enter  
+                    if [ "$firstimport" -eq "1" ]; then
+                        firstimport=0;
+                        xdotool key  --window $window F6
+                        xdotool sleep 0.1
+                        bin/xdotool key  --window $window --repeat 2 Tab
+                        xdotool sleep 0.1
+                        xdotool key  --window $window KP_Enter
+                        xdotool sleep 0.5 
+                        xdotool key  --window $window F6
+                        xdotool sleep 0.3
+                    else
+                        bin/xdotool key  --window $window --repeat 2 F6
+                        xdotool sleep 0.3
+                    fi
+                    xdotool key  --window $window KP_Enter
+                  
                     needtoexport=0
                     while read -t 0.01 -r _; do :; done
-                    xdotool sleep 5
+                    xdotool sleep 1
+                    echo "">/home/phablet/.cache/whatslectron.pparent/downloads/00000000.png
+                    xdotool sleep 4
                     while read -t 0.01 -r _; do :; done
+                    for file in /home/phablet/.cache/whatslectron.pparent/downloads/* ; do
+                        utils/rm.sh $file
+                    done
                     echo "">/home/phablet/.cache/whatslectron.pparent/downloads/00000000.png
                     utils/rm.sh /home/phablet/.local/share/whatslectron.pparent/recently-used.xbel
                 fi
@@ -69,7 +104,7 @@ xev -root  | while read -r _; do
                      read lock < /home/phablet/.cache/whatslectron.pparent/exportlock
                     if [ "$lock" != "lock" ]; then
                         echo "lock" > /home/phablet/.cache/whatslectron.pparent/exportlock
-                       ( qmlscene utils/download-helper/qml/ExportPage.qml -I  utils/download-helper/; echo "" >/home/phablet/.cache/whatslectron.pparent/exportlock; echo "">/home/phablet/.cache/whatslectron.pparent/downloads/00000000.png)  &
+                       ( qmlscene utils/download-helper/qml/ExportPage.qml -I  utils/download-helper/; echo "" >/home/phablet/.cache/whatslectron.pparent/exportlock; echo "">/home/phablet/.cache/whatslectron.pparent/downloads/00000000.png )  &
                        xdotool sleep 5;
                        utils/rm.sh /home/phablet/.local/share/whatslectron.pparent/recently-used.xbel
                     fi
